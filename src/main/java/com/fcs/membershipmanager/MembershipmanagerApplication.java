dab2c4b639a6032c97ca78003f69b41dd6592967
Fernanda Costa <fernandacosta@alu.ufc.br>
2023-09-22 14:43:59 -0300

    starting project

---
+++ src/main/java/com/fcs/membershipmanager/MembershipmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fcs.membershipmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcs/membershipmanager/MembershipmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcs.membershipmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
