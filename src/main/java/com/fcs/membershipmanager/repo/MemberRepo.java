88fd19d27044b2f0152cf5d484e056ee8f66b4e2
Fernanda Costa <fernandacosta@alu.ufc.br>
2023-10-03 21:06:49 -0300

    add service class

--- src/main/java/com/fcs/membershipmanager/repo/MemberRepo.java
+++ src/main/java/com/fcs/membershipmanager/repo/MemberRepo.java
@@ -3,6 +3,11 @@ package com.fcs.membershipmanager.repo;
 import com.fcs.membershipmanager.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepo extends JpaRepository<Member, Long> {
 
+    void deleteMemberById(Long id);
+
+    Optional findMemberById(Long id);
 }
--- src/main/java/com/fcs/membershipmanager/service/MemberService.java
+++ src/main/java/com/fcs/membershipmanager/service/MemberService.java
@@ -5,7 +5,9 @@ import com.fcs.membershipmanager.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.UUID;
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -19,5 +21,20 @@ public class MemberService {
     public Member addMember( Member member) {
         member.setMemberCode(UUID.randomUUID().toString());
         return memberRepo.save(member);
+    }
+
+    public List<Member> findAllMembers() {
+        return memberRepo.findAll();
+    }
+
+    public Member updateMember(Member member) {
+        return memberRepo.save(member);
+    }
+
+    public Member findMemberById(Long id) {
+        return memberRepo.findMemberById(id).orElseThrow(() -> new UserNotFoundException("User by id" + id + "was not find"));
+    }
+    public void deleteMember(Long id) {
+        memberRepo.deleteMemberById(id);
     }
 }
